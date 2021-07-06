a8d5280657bcb8a6dee7b53e89908bba7029fa20
Boris Kov <bkovesh@gmail.com>
2021-07-06 15:37:43 +0300

    Main page adaptive style

--- src/pages/PageMain/TopBlock/index.tsx
+++ src/pages/PageMain/TopBlock/index.tsx
@@ -1,10 +1,13 @@
 import React from 'react';
+import useMedia from 'use-media';
 
 import Button from '../../../components/Button/index';
 
 import s from './TopBlock.module.scss';
 
 const TopBlock: React.FC = () => {
+  const isMobile = useMedia({ maxWidth: 768 });
+
   return (
     <section className={s.top}>
       <div className={s.bg_img}>
@@ -20,12 +23,13 @@ const TopBlock: React.FC = () => {
                 BSC
               </div>
               <div className={s.buttons}>
-                <Button big to="/pools">
+                <Button big fullWidth={isMobile} to="/pools">
                   View All Pools
                 </Button>
                 <Button
                   href="https://exchange.pancakeswap.finance/#/swap?outputCurrency=0xb698ac9bc82c718d8eba9590564b9a5aa53d58e6"
                   big
+                  fullWidth={isMobile}
                 >
                   Buy Less
                 </Button>
