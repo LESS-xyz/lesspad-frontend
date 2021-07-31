e471eba801b577303b88f195e3b1fa5464a8ab87
Boris Kov <bkovesh@gmail.com>
2021-07-31 16:24:02 +0300

    Add block instead partners on main page

--- src/pages/PageMain/index.tsx
+++ src/pages/PageMain/index.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
 
-import logo1 from '../../assets/img/sections/alumni-logos/logo-1.svg';
+import Button from '../../components/Button';
+// import logo1 from '../../assets/img/sections/alumni-logos/logo-1.svg';
 import PoweredBy from '../About/PoweredBy/index';
 
 import PoolsinVoting from './PoolsinVoting/index';
-import FeaturedAlumni from './FeaturedAlumni';
+// import FeaturedAlumni from './FeaturedAlumni';
 import FeaturedProject from './FeaturedProjects';
 import Features from './Features';
 import Tiers from './Tiers';
@@ -12,7 +13,7 @@ import TopBlock from './TopBlock';
 
 import s from './PageMain.module.scss';
 
-const partnersLogos = [logo1, logo1, logo1, logo1, logo1, logo1, logo1, logo1];
+// const partnersLogos = [logo1, logo1, logo1, logo1, logo1, logo1, logo1, logo1];
 
 const PageMain: React.FC = () => {
   return (
@@ -23,7 +24,24 @@ const PageMain: React.FC = () => {
       <PoolsinVoting />
       <Features />
       <Tiers />
-      <FeaturedAlumni logos={partnersLogos} title="Our Partners" />
+      {/*<FeaturedAlumni logos={partnersLogos} title="Our Partners" />*/}
+
+      <div className={s.container}>
+        <div className={s.info}>
+          <div className={s.info_inner}>
+            <div className={s.info_left}>
+              We aim to provide a seamless experience for fund raising and exposure on any major
+              protocol
+            </div>
+            <div className={s.info_right}>
+              <div className={s.info_right__text}>
+                Funding? Exposure? <br /> LessPad has it all, click below!
+              </div>
+              <Button>Start application</Button>
+            </div>
+          </div>
+        </div>
+      </div>
     </main>
   );
 };
