3a51b7f94956167e83bb84730a92483199aed6a4
TheJuze <dimanikus0106@gmail.com>
2021-08-25 15:47:12 +0300

    splitted logic in progress bar

--- src/pages/PagePool/Pool/index.tsx
+++ src/pages/PagePool/Pool/index.tsx
@@ -1949,7 +1949,8 @@ const Pool: React.FC = () => {
         </div>
         <div className="grow-info">
           <div className="grow-min">
-            {!isCertified && !whitelist.length && +hardCapInTokens !== tokensShouldBeSold && (
+            {((!isCertified && +hardCapInTokens !== tokensShouldBeSold) ||
+              (isCertified && !isWhitelist && +hardCapInTokens !== tokensShouldBeSold)) && (
               <div>
                 {tiers[+currentTier - 1] || 'Current'} stage completion{' '}
                 {prettyNumber(percentageOfTokensSoldInCurrentTier.toString()) || 0}%
@@ -1960,7 +1961,8 @@ const Pool: React.FC = () => {
             </div>
           </div>
           <div className="grow-max">
-            {!isCertified && !whitelist.length && +hardCapInTokens !== tokensShouldBeSold && (
+            {((!isCertified && +hardCapInTokens !== tokensShouldBeSold) ||
+              (isCertified && !isWhitelist && +hardCapInTokens !== tokensShouldBeSold)) && (
               <div>
                 {tokensSoldInCurrentTier || 0} /{' '}
                 {prettyNumber(`${tokensShouldBeSold}`) || prettyNumber(hardCapInTokens)}{' '}
