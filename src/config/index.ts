b2e6066d96491294e53e9a1fd93622e1f75a9855
Boris Kov <bkovesh@gmail.com>
2021-08-31 17:31:21 +0300

    Newc contracts

--- src/config/index.ts
+++ src/config/index.ts
@@ -264,7 +264,7 @@ export default {
         Staking: '0xE4D00cA4eceFB3bEE229FFf97ffF3de3bBAD4B4B',
         LessLibrary: '0xa78C93A963C7C1d786EFf8CFbf54f37E0E9c12E1',
         PresaleFactory: '0xa8FD2169DFf71c22cB011c07196728B9FB75C58f',
-        PresaleFactoryCertified: '0xf8aD5Fb021c88404cE92eC7BAAE1Cf4778a3941a',
+        PresaleFactoryCertified: '0xe5B14ea382298d458849881e2aD8E9419dA14cc3',
       },
     },
   },
